b1e8122c64e9ded98f0a307d8c5f950dd4249e63
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-05-26 01:16:00 +0400

    views

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Post
 
-# Create your views here.
+class PostView(View):
+    '''records output'''
+    def get(self, request):
+        post = Post.objects.all()
+        return render(request, 'blog/blog.html', {'post_list': post})
+
+
